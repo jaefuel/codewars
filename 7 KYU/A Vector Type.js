b36f68b4178554115ae79379cc55f2f5fc497fbc
Cris Jacobo <crisjofficial@gmail.com>
2022-07-27 20:08:44 -0500

    finished 7 kyu

---
+++ 7 KYU/A Vector Type.js
@@ -0,0 +1,24 @@
+class Vec 
+{
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+        
+    }
+
+    plus(v)
+    {
+        return new Vec(this.x + v.x, this.y + v.y);
+    }
+
+    minus(v)
+    {
+        return new Vec(this.x - v.x, this.y - v.y);
+    }
+
+    get length()
+    {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+}
